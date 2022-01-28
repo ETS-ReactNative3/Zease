051594cedcccc632f6c183dc4fb429e33b0b4f20
Sam Kanan <samkanan@gmail.com>
2022-01-28 14:58:51 -0600

    reconfigured firebase

--- firebase.js
+++ firebase.js
@@ -1,20 +1,10 @@
-// Import the functions you need from the SDKs you need
+import firebaseConfig from './firebaseConfig';
 import * as firebase from 'firebase';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyBmS_S3Ae6skCQa-ZkaBsmoeOFOx7Ms2eA',
-  authDomain: 'capstone-8c19d.firebaseapp.com',
-  databaseURL: 'https://capstone-8c19d-default-rtdb.firebaseio.com',
-  projectId: 'capstone-8c19d',
-  storageBucket: 'capstone-8c19d.appspot.com',
-  messagingSenderId: '718930949173',
-  appId: '1:718930949173:web:8c07fc54da24732595ed7c',
-  measurementId: 'G-YXMLPCC99R'
-};
 
 // Initialize Firebase
 let app;
