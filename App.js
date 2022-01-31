4e3254b7c3546e58dfa8fa0fde473c80385522b2
Liz Yoder <EAHYoder@gmail.com>
2022-01-31 11:36:19 -0600

    added separate chart view separate from app

--- App.js
+++ App.js
@@ -1,73 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useEffect, useState } from "react";
-import { VictoryChart, VictoryScatter } from "victory-native";
-// import LoginScreen from "./screens/LoginScreen";
-// import HomeScreen from "./screens/HomeScreen";
-// import Chart from "./screens/Chart";
-import { database } from "./firebase";
+
+import LoginScreen from "./screens/LoginScreen";
+import HomeScreen from "./screens/HomeScreen";
+import Chart from "./screens/Chart";
+
 const Stack = createNativeStackNavigator();
 
-const dummyData = [
-  {
-    date: new Date(2022, 1, 1),
-    sleepLength: 420,
-    quality: "poor",
-    factors: ["screentime", "caffeine"],
-  },
-  {
-    date: new Date(2022, 1, 2),
-    sleepLength: 430,
-    quality: "ok",
-    factors: ["screentime", "caffeine"],
-  },
-  {
-    date: new Date(2022, 1, 3),
-    sleepLength: 440,
-    quality: "ok",
-    factors: ["caffeine"],
-  },
-];
-
 export default function App() {
-  //make it so we store data from firebase in our local state.
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    //get data from firebase.  the reference for the data was established when it was written into the database.  This is getting a "snapshot" of the data
-    const sleepEntriesRef = database.ref("simplesleepEntries");
-
-    //this on method gets the value of the data at that reference.
-    sleepEntriesRef.on("value", (snapshot) => {
-      const sleepEntryData = snapshot.val();
-      //Im trying to put the data on local state so that it can be passed to the chart view.  this isn't working.  I'm not sure why.
-      setData(sleepEntryData);
-    });
-  }, []);
-
-  const reformatDataForChart = (dbData) => {
-    return dbData.map((dbSleepEntry) => {
-      return {
-        SleepLength: dbSleepEntry.length,
-        SleepQuality: dbSleepEntry.quality,
-      };
-    });
-  };
-
   return (
-    <NavigationContainer>
-      <VictoryChart>
-        {data && (
-          <VictoryScatter
-            data={reformatDataForChart(data)}
-            x="SleepLength"
-            y="SleepQuality"
+    <>
+      <Chart />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Login"
+            component={LoginScreen}
           />
-        )}
-      </VictoryChart>
-    </NavigationContainer>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
--- screens/Chart.js
+++ screens/Chart.js
@@ -1,11 +1,63 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import React from "react";
+import { useEffect, useState } from "react";
+import { VictoryChart, VictoryScatter } from "victory-native";
+import { database } from "../firebase";
+
+// const dummyData = [
+//   {
+//     date: new Date(2022, 1, 1),
+//     sleepLength: 420,
+//     quality: "poor",
+//     factors: ["screentime", "caffeine"],
+//   },
+//   {
+//     date: new Date(2022, 1, 2),
+//     sleepLength: 430,
+//     quality: "ok",
+//     factors: ["screentime", "caffeine"],
+//   },
+//   {
+//     date: new Date(2022, 1, 3),
+//     sleepLength: 440,
+//     quality: "ok",
+//     factors: ["caffeine"],
+//   },
+// ];
 
 const Chart = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    //get data from firebase.  the reference for the data was established when it was written into the database.  This is getting a "snapshot" of the data
+    const sleepEntriesRef = database.ref("simplesleepEntries");
+
+    //this on method gets the value of the data at that reference.
+    sleepEntriesRef.on("value", (snapshot) => {
+      const sleepEntryData = snapshot.val();
+      //Im trying to put the data on local state so that it can be passed to the chart view.  this isn't working.  I'm not sure why.
+      setData(sleepEntryData);
+    });
+  }, []);
+
+  const reformatDataForChart = (dbData) => {
+    return dbData.map((dbSleepEntry) => {
+      return {
+        SleepLength: dbSleepEntry.length,
+        SleepQuality: dbSleepEntry.quality,
+      };
+    });
+  };
   return (
-    <View>
-      <Text>A chart could go here</Text>
-    </View>
+    <VictoryChart>
+      {data && (
+        <VictoryScatter
+          data={reformatDataForChart(data)}
+          x="SleepLength"
+          y="SleepQuality"
+        />
+      )}
+    </VictoryChart>
   );
 };
 
