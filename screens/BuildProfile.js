a6c3d9302d26a485e537fb67e5e7098ec95bc472
Liz Yoder <EAHYoder@gmail.com>
2022-02-01 15:44:28 -0600

    added time picker library and started to add first time picker input

--- screens/BuildProfile.js
+++ screens/BuildProfile.js
@@ -1,14 +1,28 @@
-import { StyleSheet, View, Text, TextInput } from "react-native";
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
 import React from "react";
 import { useEffect, useState } from "react";
 import { database } from "../firebase";
 import tw from "tailwind-react-native-classnames";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 const BuildProfile = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [name, setName] = useState("");
+  const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
+  const [sleepGoalStart, setsleepGoalStart] = useState(0);
+
+  const showTimePicker = () => {
+    setTimePickerVisibility(true);
+  };
+  const hideTimePicker = () => {
+    setTimePickerVisibility(fasle);
+  };
+  const handleTimeConfirm = (time) => {
+    hideTimePicker();
+  };
+
   return (
     <View style={tw`flex-1 items-center justify-center`}>
       <Text>Welcome!</Text>
@@ -34,6 +48,13 @@ const BuildProfile = () => {
           placeholder="name"
           value={name}
           onChangeText={(text) => setName(text)}
+        />
+        <Button title="Select Bed Time Goal" onPress={showTimePicker} />
+        <DateTimePickerModal
+          isVisible={isTimePickerVisible}
+          mode="time"
+          onConfirm={handleTimeConfirm}
+          onCancel={hideTimePicker}
         />
       </View>
     </View>
