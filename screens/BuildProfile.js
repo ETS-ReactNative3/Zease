9116a5d01e1777b2d5b03a1a9396605851b1bd92
Liz Yoder <EAHYoder@gmail.com>
2022-02-01 15:26:02 -0600

    tailwind react native classnames are working, text inputs for email, password, confirm password, and name are all working

--- screens/BuildProfile.js
+++ screens/BuildProfile.js
@@ -1,16 +1,43 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TextInput } from "react-native";
 import React from "react";
 import { useEffect, useState } from "react";
 import { database } from "../firebase";
+import tw from "tailwind-react-native-classnames";
 
 const BuildProfile = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [name, setName] = useState("");
   return (
-    <View>
-      <Text>Create Profile Form goes here</Text>
+    <View style={tw`flex-1 items-center justify-center`}>
+      <Text>Welcome!</Text>
+      <View>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry
+        />
+        <TextInput
+          placeholder="passwordConfirm"
+          value={passwordConfirm}
+          onChangeText={(text) => setPasswordConfirm(text)}
+          secureTextEntry
+        />
+        <TextInput
+          placeholder="name"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+      </View>
     </View>
   );
 };
 
 export default BuildProfile;
-
-const styles = StyleSheet.create({});
