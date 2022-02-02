36de680422ec8e89a055bac3d644830a2667c07a
Liz Yoder <EAHYoder@gmail.com>
2022-02-02 15:39:38 -0600

    made attempt at functionality needed for submit button with some form validation

--- screens/BuildProfile.js
+++ screens/BuildProfile.js
@@ -1,18 +1,21 @@
 import {
-  StyleSheet,
   View,
   Text,
   TextInput,
   Button,
   Switch,
+  TouchableOpacity,
+  Alert,
 } from "react-native";
 import React from "react";
 import { useEffect, useState } from "react";
-import { database } from "../firebase";
+import { auth, database } from "../firebase";
 import tw from "tailwind-react-native-classnames";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import SleepFactorCategory from "./SleepFactorCategory";
+
 const dummySleepFactors = {
   1: {
     name: "Caffeine",
@@ -99,16 +102,7 @@ const BuildProfile = () => {
     return `${hoursString}:${minString} ${AmPm}`;
   };
 
-  const handleBedTimeConfirm = (time) => {
-    setsleepGoalStart(convertToMilitaryString(time));
-    setBedTimePickerVisibility(false);
-  };
-
-  const handleWakeTimeConfirm = (time) => {
-    setsleepGoalEnd(convertToMilitaryString(time));
-    setWakeTimePickerVisibility(false);
-  };
-
+  //takes in the object of all sleep factors fetched from realtime firebase and reformats it into an array of categories that can be mapped into sleep factor category sub components.
   const reformatFactors = (dbFactorsObject) => {
     const categories = [
       {
@@ -139,8 +133,84 @@ const BuildProfile = () => {
           break;
       }
     }
-
     return categories;
+  };
+
+  const handleBedTimeConfirm = (time) => {
+    setsleepGoalStart(convertToMilitaryString(time));
+    setBedTimePickerVisibility(false);
+  };
+
+  const handleWakeTimeConfirm = (time) => {
+    setsleepGoalEnd(convertToMilitaryString(time));
+    setWakeTimePickerVisibility(false);
+  };
+
+  const handleSubmit = async () => {
+    let validated = true;
+
+    if (password !== passwordConfirm) {
+      Alert.alert("Error", "Password and Confirm Password do not match.");
+      validated = false;
+    }
+
+    try {
+      //get the user's selected sleep factors from async storage
+      const userFactorsString = await AsyncStorage.getItem("userFactors");
+
+      const userFactors = userFactorsString
+        ? JSON.parse(userFactorsString)
+        : [];
+
+      //make sure that all required fields are filled in
+      if (
+        email === "" ||
+        name === "" ||
+        sleepGoalStart === "" ||
+        sleepGoalEnd === "" ||
+        userFactors.length
+      ) {
+        Alert.alert("Error", "Please fill in all required fields.");
+        validated = false;
+      }
+
+      if (validated) {
+        putUserinDB(userFactors);
+      }
+    } catch (error) {
+      console.log(
+        "there was an error in fetching the user's sleep factors from async storage: ",
+        error
+      );
+    }
+  };
+
+  const putUserinDB = async (userFactors) => {
+    try {
+      let newUser = {
+        email,
+        name,
+        sleepGoalStart,
+        sleepGoalEnd,
+        userFactors,
+        logReminderOn,
+        sleepReminderOn,
+      };
+      console.log("newUser about to be added to db", newUser);
+
+      auth
+        .createUserWithEmailAndPassword(email, password)
+        .then((userCredentials) => {
+          const userId = userCredentials.user.uid;
+          database.ref("users/" + userId).set(newUser);
+        })
+        .catch((error) => alert(error.message));
+    } catch (error) {
+      console.log(
+        "there was an error in attempting to add this user to the database: ",
+        error
+      );
+    }
   };
 
   return (
@@ -223,6 +293,9 @@ const BuildProfile = () => {
             <SleepFactorCategory key={category.name} category={category} />
           );
         })}
+        <TouchableOpacity onPress={handleSubmit}>
+          <Text>Submit</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
