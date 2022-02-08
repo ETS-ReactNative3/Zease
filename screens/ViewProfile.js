8522a412cf097abfea11ea83b35e9de998bf74e1
Sam Kanan <samkanan@gmail.com>
2022-02-08 10:43:26 -0600

    added stying for view profile screen

--- screens/ViewProfile.js
+++ screens/ViewProfile.js
@@ -52,46 +52,59 @@ const AddEntry = ({ navigation }) => {
     <View style={styles.container}>
       {profileData.name && (
         <View style={styles.contentContainer}>
-          <Text style={styles.header}>Your Profile</Text>
+          <Text style={tw`font-bold text-3xl text-white mb-5 text-center`}>Your Profile</Text>
           <View style={styles.accountContainer}>
             <View style={styles.accountItem}>
-              <Text>Name: </Text>
-              <Text>{profileData.name}</Text>
+              <Text
+                style={tw`font-semibold text-white`}
+              >{`Name:                                     `}</Text>
+              <Text style={tw`font-extrabold text-white`}>{profileData.name}</Text>
             </View>
             <View style={styles.accountItem}>
-              <Text>Email: </Text>
-              <Text>{auth.currentUser.email}</Text>
+              <Text
+                style={tw`font-semibold text-white`}
+              >{`Email:                                      `}</Text>
+              <Text style={tw`font-extrabold text-white`}>{auth.currentUser.email}</Text>
             </View>
             <View style={styles.accountItem}>
-              <Text>Bed Time Goal: </Text>
-              <Text>{convertToAmPm(profileData.sleepGoalStart)}</Text>
+              <Text style={tw`font-semibold text-white`}>
+                {`Bed Time Goal:                    `}{' '}
+              </Text>
+              <Text style={tw`font-extrabold text-white`}>
+                {convertToAmPm(profileData.sleepGoalStart)}
+              </Text>
             </View>
             <View style={styles.accountItem}>
-              <Text>Wake Up Goal: </Text>
-              <Text>{convertToAmPm(profileData.sleepGoalEnd)}</Text>
+              <Text style={tw`font-semibold text-white`}>
+                {`Wake Up Goal:                     `}{' '}
+              </Text>
+              <Text style={tw`font-extrabold text-white`}>
+                {convertToAmPm(profileData.sleepGoalEnd)}
+              </Text>
             </View>
             <View style={styles.accountItem}>
-              <Text>Morning Log Reminder: </Text>
-              <Text>{profileData.logReminderOn ? 'On' : 'Off'}</Text>
+              <Text style={tw`font-semibold text-white`}>{`Morning Log Reminder:     `} </Text>
+              <Text style={tw`font-extrabold text-white`}>
+                {profileData.logReminderOn ? 'On' : 'Off'}
+              </Text>
             </View>
             <View style={styles.accountItem}>
-              <Text>Evening Sleep Reminder: </Text>
-              <Text>{profileData.sleepReminderOn ? 'On' : 'Off'}</Text>
+              <Text style={tw`font-semibold text-white`}>{`Evening Sleep Reminder:   `}</Text>
+              <Text style={tw`font-extrabold text-white`}>
+                {profileData.sleepReminderOn ? 'On' : 'Off'}
+              </Text>
             </View>
           </View>
-          <Text style={styles.header}>Your Sleep Factors</Text>
-          <View style={styles.factorsContainer}>
+          <Text style={tw`font-semibold text-white mt-10 mb-2`}>Your Sleep Factors:</Text>
+          <View>
             {Object.keys(profileData.userFactors).map((key) => (
-              <Text key={key} style={styles.factorItem}>
+              <Text key={key} style={tw`font-extrabold text-white`}>
                 {profileData.userFactors[key].name}
               </Text>
             ))}
           </View>
         </View>
       )}
-      {/* OLD BUTTON STYLES */}
-      {/* <Button style={styles.buttonContainer} title='Edit Profile' onPress={handleEdit} /> */}
-      {/* <Button title='Log Out' onPress={handleLogOut} /> */}
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.button} onPress={handleEdit}>
           <Text style={styles.buttonText}>Edit Profile</Text>
@@ -116,18 +129,12 @@ const styles = StyleSheet.create({
   contentContainer: {
     width: '80%'
   },
-  header: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    alignSelf: 'flex-start',
-    marginTop: 20
-  },
   accounttContainer: {
     paddingLeft: 5
   },
   accountItem: {
     flexDirection: 'row',
-    paddingTop: 5
+    paddingTop: 10
   },
   button: {
     alignItems: 'center',
@@ -144,11 +151,5 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'white',
     fontWeight: 'bold'
-  },
-  factorsContainer: {
-    paddingLeft: 5
-  },
-  factorItem: {
-    paddingTop: 5
   }
 });
