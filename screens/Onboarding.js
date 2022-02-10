b670421093acf963a4587ce163e11d950f77523d
Liz Yoder <EAHYoder@gmail.com>
2022-02-10 13:08:06 -0600

    added text for the oboarding slides

--- screens/Onboarding.js
+++ screens/Onboarding.js
@@ -7,7 +7,7 @@ import {
 } from "react-native";
 import React, { useState } from "react";
 import OnboardingSlide from "./OnboardingSlide";
-
+import { useSelector } from "react-redux";
 import tw from "tailwind-react-native-classnames";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -15,32 +15,38 @@ const slides = [
   {
     id: 0,
     imageSource: require("../assets/onboarding/slide1.png"),
-    slideText: "This explains that first picture",
+    slideText:
+      "Congratulations on setting up your profile!  If you ever need to update it or log out, visit the Profile tab.",
   },
   {
     id: 1,
     imageSource: require("../assets/onboarding/slide2.png"),
-    slideText: "This explains that second picture",
+    slideText:
+      "Once you've made your first sleep entry you can review it - and any later entries - by looking at all sleep entries.  However you'll only be able to edit the entry about last night's sleep.",
   },
   {
     id: 2,
     imageSource: require("../assets/onboarding/slide3.png"),
-    slideText: "This explains that third picture",
+    slideText:
+      "Once you've made two sleep entries you can analyze the data.  If you need help understanding a chart, use the information icon.",
   },
   {
     id: 3,
     imageSource: require("../assets/onboarding/slide4.png"),
-    slideText: "This explains that fourth picture",
+    slideText:
+      "It all starts with making sleep entries!  You can only make one sleep entry every day, and it must be about your sleep from last night.",
   },
 ];
 
 const Onboarding = ({ navigation }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const userName = useSelector((state) => state.profile.name) || "name";
 
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="padding">
+    <KeyboardAvoidingView style={styles.container}>
       <Text style={tw`text-white text-4xl font-black`}>
-        {<Ionicons name={"bed"} size={36} color={"white"} />}onboarding here
+        {<Ionicons name={"bed"} size={36} color={"white"} />}Welcome, {userName}
+        !
       </Text>
       <OnboardingSlide slideInfo={slides[currentSlide]} />
       <View style={tw`flex-row `}>
@@ -58,7 +64,7 @@ const Onboarding = ({ navigation }) => {
         style={styles.button}
         onPress={() => navigation.navigate("NavBar")}
       >
-        <Text>Start using the app!</Text>
+        <Text style={styles.buttonText}>Make first sleep entry</Text>
       </TouchableOpacity>
     </KeyboardAvoidingView>
   );
@@ -71,6 +77,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     opacity: 0.95,
+    paddingTop: 60,
   },
   slideSelector: {
     borderRadius: 50,
@@ -83,9 +90,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#F78A03",
     paddingVertical: 12,
-    width: 150,
+    width: 170,
     marginVertical: 10,
     borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
 
--- screens/OnboardingSlide.js
+++ screens/OnboardingSlide.js
@@ -15,13 +15,15 @@ const OnboardingSlide = ({ slideInfo }) => {
   const { imageSource, slideText } = slideInfo;
   console.log("imageSource", imageSource);
   return (
-    <View style={styles.container} behavior="padding">
+    <View style={styles.container}>
       <Image
         style={{ width: 300, height: 300, borderRadius: 15 }}
         source={imageSource}
       />
 
-      <Text style={tw`text-white text-lg font-black p-10`}>{slideText}</Text>
+      <Text style={tw`text-white text-lg font-black p-10 h-64`}>
+        {slideText}
+      </Text>
     </View>
   );
 };
@@ -29,10 +31,10 @@ const OnboardingSlide = ({ slideInfo }) => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#1C3F52",
-    flex: 1,
     justifyContent: "center",
     alignItems: "center",
     opacity: 0.95,
+    paddingTop: 30,
   },
   button: {
     alignItems: "center",
