0b2ca3277999cd319325a346adda3b5f0c0805ae
Liz Yoder <EAHYoder@gmail.com>
2022-02-24 14:38:40 -0600

    added styling to slide and informative modal on how to use it

--- screens/ChartC.js
+++ screens/ChartC.js
@@ -17,6 +17,7 @@ import { setMargin } from "../store/pieChartSliderMargin";
 
 const ChartC = ({ data }) => {
   const [showInfoModal, setShowInfoModal] = useState(false);
+  const [showMarginModal, setShowMarginModal] = useState(false);
   const [selectedPie, setSelectedPie] = useState("start");
 
   const dispatch = useDispatch();
@@ -87,10 +88,23 @@ const ChartC = ({ data }) => {
           style={{ labels: { fill: "white", fontSize: 16, fontEight: "bold" } }}
         />
       )}
+      <Text style={tw` font-bold p-2 text-center`}>
+        {" "}
+        Time Margin: {margin} minutes
+      </Text>
+      <View style={tw`flex-row`}>
+        <TouchableOpacity onPress={() => setShowMarginModal(true)}>
+          <Ionicons
+            style={tw` mt-1 pl-10 self-start`}
+            name="information-circle-outline"
+            size={25}
+          />
+        </TouchableOpacity>
+        <Text
+          style={tw`pt-2 font-semibold text-center`}
+        >{`Adjust margin for wake up or bedtime goal (min):`}</Text>
+      </View>
 
-      <Text
-        style={tw`font-semibold text-white mb-4 mt-7`}
-      >{`Adjust margin for meeting wake up or bedtime goal (min):`}</Text>
       <Slider
         step={1}
         minimumValue={1}
@@ -113,7 +127,12 @@ const ChartC = ({ data }) => {
           <View style={tw`p-4`}>
             <Text style={tw`p-4 text-base`}>
               This pie chart shows how often you are going to sleep (or waking
-              up) within fifteen minutes of your goal.
+              up) within your chosen time margin of your goal.
+            </Text>
+            <Text style={tw`p-4 text-base`}>
+              For example, if you set the margin to 10 minutes, and your bedtime
+              goal is 9:30 pm then you will have met the goal if you go to bed
+              between 9:20 pm and 9:40 pm.
             </Text>
             <Text style={tw`p-4 text-base`}>
               Toggling through the different time windows can show the progress
@@ -122,6 +141,33 @@ const ChartC = ({ data }) => {
           </View>
           <TouchableOpacity
             onPress={() => setShowInfoModal(!showInfoModal)}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+      <Modal
+        transparent={false}
+        animationType="slide"
+        visible={showMarginModal}
+        onRequestClose={() => setShowMarginModal(!showMarginModal)}
+        style={{ backgroundColor: "#3e8cb6" }}
+      >
+        <View style={tw`flex-1 items-center justify-center`}>
+          <View style={tw`p-4`}>
+            <Text style={tw`p-4 text-base`}>
+              The margin for meeting your goal is how close you need to be to
+              the goal to meet it.
+            </Text>
+            <Text style={tw`p-4 text-base`}>
+              For example, if you set the margin to 10 minutes, and your bedtime
+              goal is 9:30 pm then you will have met the goal if you go to bed
+              between 9:20 pm and 9:40 pm.
+            </Text>
+          </View>
+          <TouchableOpacity
+            onPress={() => setShowMarginModal(!showMarginModal)}
             style={styles.button}
           >
             <Text style={styles.buttonText}>Close</Text>
